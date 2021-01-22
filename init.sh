6fec5be05ca41063780b1ea0365fc5d77abc808a
Diwamoto <daiki.work.0118@gmail.com>
2021-01-22 15:15:55 +0900

    #1 シンボリックリンクで管理する実装に変更、初期設定スクリプトを作った

--- init.sh
+++ init.sh
@@ -1,18 +1,29 @@
-# サブモジュールの準備
-git submodule update -i
-git submodule update --remote
-# vagrantfileの準備
-cp Vagrantfile.default Vagrantfile
+
+# 上ディレクトリにdocker-lampをcloneしてシンボリックリンクを貼る
+cd ..
+git clone https://github.com/Diwamoto/docker-lamp.git
+cd vagrant-lamp
+ln -s ../docker-lamp docker-lamp
+
+# ローカルホストのhttps化
+cd docker-lamp/web/ssl/
+./initssl.sh
+
 # 設定ファイルの準備
-cd docker-lamp/web/php7
+cd ../php7
 cp httpd.conf.default httpd.conf
 cp php.ini.default php.ini
-# vagrantを起動
-cd ../..
-vagrant up
 
 # dnsmasqの準備
 brew install dnsmasq
-touch /etc/resolver/localhost
-echo 'nameserver 127.0.0.1' > /etc/resolver/localhost
+sudo touch /etc/resolver/localhost
+sudo echo 'nameserver 127.0.0.1' > /etc/resolver/localhost
+sudo echo 'address=/localhost/127.0.0.1' >> /usr/local/etc/dnsmasq.conf
 brew services restart dnsmasq
+
+# vagrantを起動
+cd ../../..
+cp Vagrantfile.default Vagrantfile
+vagrant up
+
+
